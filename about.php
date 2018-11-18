83a422c70a48bdcda2690c4ef3c6ff18984e507d
Nikhil2402 <ahirkarnick@gmail.com>
2018-11-18 10:45:55 +0530

    UI php

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ABOUT | RTO MAHARASHTA</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
+		<!--[if IE 6]><link rel="stylesheet" type="text/css" href="styles/ie6.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<div id="section">
+					<div>
+						<a href="index.php"><img src="images/RTO.png" alt=" " height = 70% width = 70%  /></a>
+					</div>
+					<font color = 'White' size = 5><br>Commissionerate of Transport<br>
+					</font>
+					<font color = 'White' size = 3>Department of Port and Transport, Government of Maharashtra
+					</font>
+					<span>
+						<a href="Login.php">Login Here</a>
+					</span> 
+				</div>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li class="current"><a href="about.php">About Us</a></li>
+					<li><a href="admissions.php">Driving Licence</a></li>
+					<li><a href="training-courses.php">Vehicle Registeration</a></li>
+					<li><a href="blog.php">RTO Office</a></li>
+				</ul>
+			</div>
+			<div id="content">
+				<div>
+					<h3>About Transport Department</h3>
+					<div class="first">
+						<p>The Transport Department of Government of Maharashtra is entrusted with the responsibility of providing an efficient public transportation system, control of vehicular pollution, registration of vehicles in Maharashtra, issuance of Driving licences, issuance of various permits, collection of road taxes. The department also entrusted in policy-making, co-ordination, implementation, monitoring and regulatory functions of all the Transport related aspects of Maharashtra.
+						<br><br>The Transport Department is regulated by the Government of Maharashtra in terms of policy formulation and its implementation. The Department is administered by the Transport Commissioner who is the Head of the Transport Department. </p>
+					</div>
+					<div>
+						<h2>Our Vision</h2>
+						<p>"To provide and facilitate transport - related services to the people with a thrust on speed, safety, environment-friendliness and fuel-efficiency in a transparent and citizen-friendly fashion, thus accelerating the growth and development and thereby furthering the interests of the State and Nation."</p>
+					</div>
+					<div>
+						<h2>Our Mission</h2>
+						<p>1) To aim for stricter compliance of provisions of Motor Vehicle Act & Rules there under
+						<br>2) To establish user friendly front end facilities for visiting public
+						<br>3) To strengthen the Department by providing equipments, manpower & Training
+						<br>4) To simplify the taxation of vehicles
+						<br>5) To create effective grievance redressed mechanism
+						</p>
+					</div>
+				</div>
+			</div>
+			<div id="footer">
+				<div>
+					<div class="section">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About Us</a></li>
+							<li><a href="admissions.php">Driving Licence</a></li>
+							<li><a href="training-courses.php">Vehicle Registeration</a></li>
+							<li class="last"><a href="blog.php">RTO Office</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ admissions.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LICENCE | RTO MAHARASHTA</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="styles/style.css" />
+        <!--[if IE 6]><link rel="stylesheet" type="text/css" href="styles/ie6.css" /><![endif]-->
+    </head>
+    <body>
+        <div id="page">
+            <div id="header">
+                <div id="section">
+                    <div>
+						<a href="index.php"><img src="images/RTO.png" alt=" " height = 70% width = 70%  /></a>
+					</div>
+					<font color = 'White' size = 5><br>Commissionerate of Transport<br>
+					</font>
+					<font color = 'White' size = 3>Department of Port and Transport, Government of Maharashtra
+					</font>
+					<span>
+						<a href="Login.php">Login Here</a>
+					</span> 
+                </div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li class="current"><a href="admissions.php">Driving Licence</a></li>
+					<li><a href="training-courses.php">Vehicle Registeration</a></li>
+					<li><a href="blog.php">RTO Office</a></li>
+                </ul>
+            </div>
+            <div id="content">
+                <div>
+                    <h3>Driving Licence</h3>
+                    <div class="first">
+						 <p>The person who wants a driving license is required to apply in the prescribed Form. The applicant is required to pass computer as well as driving test in order to get a license.</p>
+					</div>
+                    <div>
+                        <h2>Application Form</h2>
+                        <ul>
+							<li><p>An applicant is required to obtain a learning license in order to get new driving license.</p>
+							<li><p>An applicant is required to apply in Form No.2/4 for the purpose of both learning as well as driving license, Medical Certificate in Form No.1 (A) is also required to be attached.</p>
+						</ul>
+					</div>
+                    <div>
+                        <h2>Eligibility</h2>
+                        <ul>
+							<li>A person should have completed 16 years to obtain license for 2 wheelers without gear. 
+							<li>A person should have completed 18 years age to obtain license for 2 wheelers with gear, Motor-car and other non-transport vehicles.
+							<li>For transport vehicles, a person should have completed 20 years of age. In addition he should be passed standard 8th pass and should have experience of 1 year driving a light motor vehicle.
+						</ul>
+                    </div>
+					<div>
+                        <h2>Necessary Documents</h2>
+                        <ul>
+							<li><b>Proof of Age</b>: School Leaving Certificate, Birth Certificate, Passport, Pan Card, L.I.C. Policy or a Certificate from Civil Surgeon or a Doctor equivalent to that can be submitted as a proof of age.
+							<li><b>Proof of Address</b>: School Leaving Certificate, Passport, L.I.C. Policy, Electoral Voter I-Card, Light Bill, Telephone Bill, receipt of House Tax with address, Pay Slip of Central/State/Local self Government or an Affidavit from the applicant to that effect can be submitted asthe proof for address. 
+						</ul>
+                    </div>
+					<div>
+                        <h2>Fee</h2>
+                        <ul>
+							<li>The fee for Learning License and Driving License has to be paid together.Rs.25/-as test fee plus Rs.30/- per category of vehicle is required to be paid for Learning License.
+							<li>Rs.200/- for a Smart Card Driving License and Rs.50/- per category of vehicle is required to be paid for Driving License.						
+						</ul>
+                    </div>
+                </div>
+            </div>
+            <div id="footer">
+				<div>
+					<div class="section">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About Us</a></li>
+							<li><a href="admissions.php">Driving Licence</a></li>
+							<li><a href="training-courses.php">Vehicle Registeration</a></li>
+							<li class="last"><a href="blog.php">RTO Office</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
