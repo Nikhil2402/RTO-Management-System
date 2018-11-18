fd2ec538808d0f39166063a48879c716ce90a977
adheesh27 <adheesh1996@gmail.com>
2018-11-18 10:42:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HOME | RTO MAHARASHTRA</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
+		<!--[if IE 6]><link rel="stylesheet" type="text/css" href="styles/ie6.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<div id="section">
+					<div>
+						<a href="index.php"><img src="images/RTO.png" alt=" " height = 70% width = 70%  /></a>
+					</div>
+					<font color = 'White' size = 5><br>Commissionerate of Transport<br>
+					</font>
+					<font color = 'White' size = 3>Department of Port and Transport, Government of Maharashtra
+					</font>
+					<span>
+						<a href="Login.php">Login Here</a>
+					</span> 
+				</div>
+				<ul>
+					<li class="current"><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="admissions.php">Driving Licence</a></li>
+					<li><a href="training-courses.php">Vehicle Registeration</a></li>
+					<li><a href="blog.php">RTO Office</a></li>
+				</ul>
+			</div>
+			<div id="content">
+				<div id="home">
+					<div>
+						<div id="aside">
+							<div>
+								<h1>Welcome to <span class="last">RTO MAHARASHTRA</span></h1>
+								<a href="#"><img src="images/model1.jpg" alt="" width = 45% height = 45%/></a>
+								<p>The Transport Department of Goverment of Maharashtra is entrusted with the responsibility of providing an efficient public transportation system, control of vehicular pollution, registration of vehicles in Maharashtra, issuance of Driving licences, issuance of various permits, collection of road taxes. The department also entrusted in policy-making, co-ordination, implementation, monitoring and regulatory functions of all the Transport related aspects of Maharashtra. </p>
+								</div>
+							<ul>
+								<li><h2><a href="#">Our <span class="last">Aim</span></a></h2>
+									<p>The Transport Department is regulated by the Government of Maharashtra in terms of policy formulation and its implementation. The Department is administered by the Transport Commissioner who is the Head of the Transport Department. </p>
+								</li>
+								<li>
+									<h2><a href="#">Our <span class="last">Vision</span></a></h2>
+									<p>We Strive to aim for stricter compliance of provisions of Motor Vehicle Act & Rules there under.</p>
+									<p>We Strive to establish user friendly front end facilities for visiting public & simplify the taxation of vehicles.</p>
+								</li>
+							</ul>
+						</div>
+						<div id="calendar">
+							<h3>Upcoming Events</h3>
+							<ul>
+								<li>
+									<div> <span>10<br />
+										Jan</span>
+										<h2><a href="#">Road Safety Awareness</a></h2>
+									</div>
+									<p>Road Safety Awareness Programme is being held on 10th of January.</p>
+								</li>
+								<li>
+									<div> <span>14<br />
+										Jan</span>
+										<h2><a href="#">Dry Day</a></h2>
+									</div>
+									<p>Dry Day is being held on 14th January where only Public Transport System will be Allowed to Travel.</p>
+								</li>
+								<li>
+									<div> <span>30<br />
+										Jan</span>
+										<h2><a href="#">Server Maintenance</a></h2>
+									</div>
+									<p>Server Maintenance will occur on 30th January from 12:00 IST to 1:30 IST.</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="footer">
+				<div>
+					<div class="section">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About Us</a></li>
+							<li><a href="admissions.php">Driving Licence</a></li>
+							<li><a href="training-courses.php">Vehicle Registeration</a></li>
+							<li class="last"><a href="blog.php">RTO Office</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
