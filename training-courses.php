0fb7dcfc25d6e35f5444491e7ca45c82a161ec60
amritrajck <amritrajck@gmail.com>
2018-11-18 10:42:24 +0530

    training courses php

---
+++ training-courses.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>REGISTERATION | RTO MAHARASHTA</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="styles/style.css" />
+        <!--[if IE 6]><link rel="stylesheet" type="text/css" href="styles/ie6.css" /><![endif]-->
+    </head>
+    <body>
+        <div id="page">
+            <div id="header">
+                <div id="section">
+                    <div>
+						<a href="index.php"><img src="images/RTO.png" alt=" " height = 70% width = 70%  /></a>
+					</div>
+					<font color = 'White' size = 5><br>Commissionerate of Transport<br>
+					</font>
+					<font color = 'White' size = 3>Department of Port and Transport, Government of Maharashtra
+					</font>
+					<span>
+						<a href="Login.php">Login Here</a>
+					</span> 
+                </div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="admissions.php">Driving Licence</a></li>
+					<li class="current"><a href="training-courses.php">Vehicle Registeration</a></li>
+					<li><a href="blog.php">RTO Office</a></li>
+                </ul>
+            </div>
+            <div id="content">
+                <div>
+                    <h3>Registration of Motor Vehicle</h3>
+                     <div>
+                        <h2>Application</h2>
+                        <ul>
+							<li>Application in Form - 20 should be made for registration of a New Vehicle. It is compulsory to submit Form - 20 online on the web portal of the department. 
+							<li>The print out of uploaded Form - 20 should be taken and should be submitted at the concerned R.T.O. along with other necessary documents. 
+						</ul>
+					</div>
+                    <div>
+                        <h2>Documents Required</h2>
+                        <ul>
+							<li>Sale Certificate in Form No.21 
+							<li>Road worthiness certificate in Form 22. 
+							<li>Identity proof with photographs of applicant. 
+							<li>Vehicle insurance certificate.  
+							<li>Proof of address 
+							<li>In case vehicle is imported, then, bill of entry submitted to Customs and in case, vehicle is not a body built vehicle, then Form No.22 Part 2 (A) should also be submitted along with the application. 
+							<li>School Leaving Certificate, Passport, L.I.C. Policy, Electoral Voter I-Card, Light Bill, Telephone Bill, receipt of House Tax with address, Pay Slip of Central/State/Local self Government or an Affidavit from the applicant to that effect can be submitted as a proof of address as well as proof of identification.  
+						</ul>
+                    </div>
+					<div>
+                        <h2>Procedure of Registration of Vehicle and Issue of Smart Card RC Book</h2>
+                        <ul>
+							<li>A new vehicle is registered in the R.T.O. were the vehicle owner ordinarily resides or has his place of business. 
+							<li>The Form No. 20 as downloaded from the website, after submitting it online along with necessary documents and proof of payment of tax and fee should be submitted at the concerned R.T.O. 
+							<li>The inspection of two wheelers and car is permitted to be done by authorized dealer in the State. In case, such certificate is issued by the dealer, the same should be attached with the application; otherwise the vehicle is required to be produced in R.T.O. for inspection. 
+							<li>If, all papers attached to Form-20 are found correct, then, a registration number is allocated to the vehicle, Unless a selected number has been obtained by the owner paying appropriate fee, the vehicle registration number is allocated by computer through a random system after the registration process is completed. 
+							<li>The vehicle owner is informed about the registration number through SMS or E-Mail address submitted by them. 
+							<li>The smart R.C. Book of the vehicle is sent by speed-post at the address indicated by the applicant in the Form. 
+						</ul>
+                    </div>
+					<div>
+                        <h2>Fee for Choice Number</h2>
+                        <ul>
+							<li>A system of allocating a number of choice to a new vehicle is in operation in the State. 
+							<li>The number can be obtained by public auction, whenever a new series is opened in a R.T.O office. 
+							<li>After public auction, the remaining numbers can be selected as choice number by paying a fee of Rs.1000/- for two wheelers and Rs.5000/- for four wheelers. 
+						</ul>
+                    </div>
+                </div>
+            </div>
+           <div id="footer">
+				<div>
+					<div class="section">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About Us</a></li>
+							<li><a href="admissions.php">Driving Licence</a></li>
+							<li><a href="training-courses.php">Vehicle Registeration</a></li>
+							<li class="last"><a href="blog.php">RTO Office</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
